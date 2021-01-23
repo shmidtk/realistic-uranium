c68955f23708100d100ec522e0bbb7308e3b0482
shmidtk <shmidtk@gmail.com>
2021-01-23 23:21:47 +0300

    - Update to 1.1

--- prototypes/resources.lua
+++ prototypes/resources.lua
@@ -1,3 +1,5 @@
+local resource_autoplace = require("__core__/lualib/resource-autoplace")
+
 --[[thorium_autoplace_settings = {
   name = "thorium-ore",
   order = "b",
@@ -11,34 +13,8 @@
   resource_index = 51,
   regular_rq_factor_multiplier = 0.9}]]--
 
-
-fluorite_autoplace_settings = { --Fluorite
-    name = "fluorite-ore",
-    order = "b",
-    base_density = 2,
-    base_spots_per_km2 = 1.1,
-    has_starting_area_placement = false,
-    random_spot_size_minimum = 0.5,
-    random_spot_size_maximum = 3,
-    regular_blob_amplitude_multiplier = 1.25,
-    richness_post_multiplier = 0.75,
-    resource_index = 51,
-    regular_rq_factor_multiplier = 0.9}
-
-
 data:extend(
 {
-    { --Fluorite
-      type = "autoplace-control",
-      name = "fluorite-ore",
-      richness = true,
-      order = "b-e",
-      category = "resource"
-    },
-    {
-      type = "noise-layer",
-      name = "fluorite-ore"
-    },
     {
       type = "resource",
       name = "fluorite-ore",
@@ -52,11 +28,23 @@ data:extend(
           hardness = 1,
           mining_particle = "coal-particle",
           mining_time = 2,
-          result = "dtk-fluorite"
+          result = "dtk-fluorite",
         },
       collision_box = {{ -0.1, -0.1}, {0.1, 0.1}},
       selection_box = {{ -0.5, -0.5}, {0.5, 0.5}},
-      autoplace = resource_autoplace.resource_autoplace_settings(fluorite_autoplace_settings),
+      autoplace = resource_autoplace.resource_autoplace_settings
+      {
+        name = "fluorite-ore",
+        order = "b",
+        base_density = 2.0,
+        base_spots_per_km2 = 1.1,
+        has_starting_area_placement = false,
+        random_spot_size_minimum = 0.25,
+        random_spot_size_maximum = 3,
+        regular_blob_amplitude_multiplier = 1,
+        regular_rq_factor_multiplier = 0.9,
+        candidate_spot_count = 22
+      },
       stage_counts = {1000, 600, 400, 200, 100, 50, 20, 1},
       stages =
         {
@@ -80,5 +68,17 @@ data:extend(
               }
           }
         },
+        
+    },
+    {
+      type = "autoplace-control",
+      name = "fluorite-ore",
+      order = "k-a",
+      richness = true,
+      category = "resource"
+    },
+    {
+      type = "noise-layer",
+      name = "fluorite-ore"
     },
 })
